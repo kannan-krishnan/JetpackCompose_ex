1269c22881e34af0913cdf339b068858e2f67e11
icliniq_n6 <kannanramakrishnan>
2023-01-25 12:26:58 +0530

    Column

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -3,20 +3,35 @@ package com.example.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposeexample.ui.theme.JetpackComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+           Column( modifier= Modifier
+               .fillMaxSize()
+               .background(Color.Yellow),
 
+               horizontalAlignment = Alignment.CenterHorizontally,
+               verticalArrangement = Arrangement.SpaceEvenly
+           ) { // place item one by one
+               Text(text = "hello")
+               Text(text = "tamil")
+           }
+          /* Row() { // place  Side by side
+               Text(text = "hello")
+               Text(text = "tamil")
+           }*/
         }
     }
 }
@@ -29,7 +44,6 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeExampleTheme {
         Greeting("Android")
-    }
+
 }
